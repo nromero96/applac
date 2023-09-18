05bb918f72f492ee5e39fc1e34035450c772ed78
nromero96 <niltondeveloper96@gmail.com>
2023-09-18 09:11:57 -0500

    Eliminado Proyecto Zip que estaba comprimido

--- routes/web.php
+++ routes/web.php
@@ -52,12 +52,12 @@ Route::group(['middleware' => ['auth', 'ensureStatusActive']], function () {
     //     return 'Storage link creado correctamente en cpanel.';
     // });
 
-    // //send test mail simple text use smtp config
-    // Route::get('/enviar-correo', function () {
-    //     $destinatario = 'niltondeveloper96@gmail.com';
-    //     Mail::to($destinatario)->send(new  \App\Mail\PruebaCorreo());
-    //     return "Correo enviado desde la ruta.";
-    // });
+    //send test mail simple text use smtp config
+    Route::get('/enviar-correo', function () {
+        $destinatario = 'niltondeveloper96@gmail.com';
+        Mail::to($destinatario)->send(new  \App\Mail\PruebaCorreo());
+        return "Correo enviado desde la ruta.";
+    });
 
     // dashboard
     Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard.index');
