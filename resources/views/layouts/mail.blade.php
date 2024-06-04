06d7f413c53e06f0141294a567fc2ea6e4507517
nromero96 <niltondeveloper96@gmail.com>
2024-06-04 16:41:47 -0500

    Update email size footer text

--- resources/views/layouts/mail.blade.php
+++ resources/views/layouts/mail.blade.php
@@ -7,7 +7,7 @@
     <style>
         body {
             font-family: Arial, sans-serif;
-            background-color: #f4f4f4;
+            background-color: #E0E0E0;
             margin: 0;
             padding: 0;
         }
@@ -56,6 +56,36 @@
                 display: block !important;
                 margin: 0 auto !important;
             }
+
+            .lgfoot{
+                max-width: 70px !important;
+            }
+
+            .txtsmd{
+                font-size: 16px !important;
+                line-height: 1.1 !important;
+            }
+
+            .getaquote{
+            font-size: 12px !important;
+                line-height: 1.1 !important;
+            }
+
+            .trackcarg{
+            font-size: 12px !important;
+                line-height: 1.1 !important;
+            }
+
+            .coneus{
+                font-size: 12px !important;
+                line-height: 1.1 !important;
+            }
+
+            .redsocia img{
+                width: 23px !important;
+                height: 23px !important;
+            }
+
         }
 
     </style>
@@ -79,23 +109,23 @@
                 <table width="100%">
                     <tr>
                         <td style="padding: 14px; text-align: left;">
-                            <img src="https://app.latinamericancargo.com/assets/img/lg-white-lac-mlg.png" alt="Logo LAC" style="max-width: 101px; height: auto;">
+                            <img class="lgfoot" src="https://app.latinamericancargo.com/assets/img/lg-white-lac-mlg.png" alt="Logo LAC" style="max-width: 101px; height: auto;">
                         </td>
                         <td>
-                            <p style="font-size:20px;line-height:1.6;padding: 14px;text-align: right;">Specialization Makes a Difference</p>
+                            <p class="txtsmd" style="font-size:20px;line-height:1.6;padding: 14px;text-align: right;">Specialization Makes a Difference</p>
                         </td>
                     </tr>
                     <tr>
                         <td style="padding: 0px 14px; text-align: left;">
-                            <b><a href="https://www.latinamericancargo.com/request-shipping-quote/" style="color:#D2CDC5; font-size: 14px; line-height: 1.5;text-decoration: underline;margin-right: 10px;">Get a Quote</a> <a href="https://quemtr.webtracker.wisegrid.net/Login/Login.aspx?ReturnUrl=%2fDefault.aspx" style="color:#D2CDC5; font-size: 14px; line-height: 1.5; text-decoration: underline;">Track your Shipment</a></b>
+                            <b><a href="https://www.latinamericancargo.com/request-shipping-quote/" class="getaquote" style="color:#D2CDC5; font-size: 14px; line-height: 1.5;text-decoration: underline;margin-right: 10px;">Get a Quote</a> <a href="https://quemtr.webtracker.wisegrid.net/Login/Login.aspx?ReturnUrl=%2fDefault.aspx" class="trackcarg" style="color:#D2CDC5; font-size: 14px; line-height: 1.5; text-decoration: underline;">Track your Shipment</a></b>
                         </td>
                         <td style="padding: 0px 14px; text-align: right;">
                             <table>
                                 <tr>
                                     <td style="padding: 0px 3px 4px;">
-                                        <b style="color:#D2CDC5; font-size: 14px; line-height: 1.5;">Connect with us</b>
+                                        <b class="coneus" style="color:#D2CDC5; font-size: 14px; line-height: 1.5;">Connect with us</b>
                                     </td>
-                                    <td style="width: 95px;">
+                                    <td class="redsocia" style="width: 95px;">
                                         <a href="https://www.linkedin.com/company/latin-american-cargo/" style="color:#D2CDC5; font-size: 14px; line-height: 1.5; text-decoration: none; margin: 0px 2px 0px;"><img src="https://app.latinamericancargo.com/assets/img/mlg_linkedIn_white.png" alt="LinkedIn" style="width: 24px; height: 24px;"></a>
                                         <a href="https://www.youtube.com/user/LatinAmericanCargo" style="color:#D2CDC5; font-size: 14px; line-height: 1.5; text-decoration: none; margin: 0px 2px 0px;"><img src="https://app.latinamericancargo.com/assets/img/mlg_outube_white.png" alt="YouTube" style="width: 24px; height: 24px;"></a>
                                         <a href="https://www.facebook.com/LatinAmericanCargo/" style="color:#D2CDC5; font-size: 14px; line-height: 1.5; text-decoration: none; margin: 0px 2px 0px;"><img src="https://app.latinamericancargo.com/assets/img/mlg_facebook_white.png" alt="Facebook" style="width: 24px; height: 24px;"></a>
