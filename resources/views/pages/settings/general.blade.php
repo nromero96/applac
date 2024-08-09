a6bf8614d7be985e59d1cb1bb4471d118590bc5a
Nilton PC <niltondeveloper96@gmail.com>
2024-08-09 11:45:03 -0500

    Update text settings

--- resources/views/pages/settings/general.blade.php
+++ resources/views/pages/settings/general.blade.php
@@ -48,11 +48,13 @@
                         <form class="row g-3" action="{{ route('settings.update') }}" method="POST" enctype="multipart/form-data">
                             @csrf
                             <div class="col-md-12">
-                                <label for="users_auto_assigned_quotes" class="form-label fw-bold mb-0">{{__("Users Auto assigned quotes with less than 4 stars")}} <span class="text-danger">*</span></label>
-                                <p class="text-muted d-block">{{__("Select users who will be auto assigned quotes with less than 4 stars, if no user is selected, no user will be auto assigned.") }}</p>
+                                <label for="users_auto_assigned_quotes" class="form-label fw-bold mb-0">{{__("Active users for auto-assign.")}} <span class="text-danger">*</span></label>
+                                <p class="text-muted d-block">{{__("Select users who will be auto-assigned quotes. If no user is selected, none will be auto-assigned.") }}</p>
                                 <div class="row">
 
                                     @php
+
+                                        $num = 1;
                                         //$users_auto_assigned_quotes;
                                         $users_auto_assigned_quotes = json_decode($users_auto_assigned_quotes);
                                         //get column value
@@ -66,21 +68,33 @@
                                         <div class="col-md-6">
                                             <div class="form-check form-check-inline">
                                                 <input class="form-check-input" type="checkbox" name="users_auto_assigned_quotes[]" value="{{$user->id}}" id="users_auto_assigned_quotes{{$user->id}}" @if(in_array($user->id, $autoAssignedUserIds)) checked @endif>
-                                                <label class="form-check-label" for="users_auto_assigned_quotes{{$user->id}}">{{$user->name}} {{ $user->last_name }}</label>
+                                                <label class="form-check-label" for="users_auto_assigned_quotes{{$user->id}}">({{$num++}}) {{$user->name}} {{ $user->last_name }}</label>
                                             </div>
                                         </div>
                                     @endforeach
                                 </div>
                                 {!!$errors->first("users_auto_assigned_quotes", "<span class='text-danger'>:message</span>")!!}
+                                <small class="text-muted d-block mt-2 mb-1">{{__("The system assigns quotes based on the customer's rating and email domain, with priority given to users who have previously managed quotes from the same domain (excluding public email domains like Gmail, Outlook.com, Hotmail.com, Live.com, Aol.com, Msn.com or Yahoo).")}}</small>
+
+                                <ul class="text-muted ps-3 mt-0">
+                                    <li class=""><small>{{__("5-Star Quotes")}}: {{__("These are assigned manually by administrators.")}}</small></li>
+                                    <li class=""><small>{{__("4-Star Quotes")}}: {{__("These are distributed equally among selected users, considering the number of quotes each user has handled in the past 24 hours.")}}</small></li>
+                                    <li class=""><small>{{__("3-Star or Lower Quotes")}}: {{__("These are assigned in a rotating manner, following the predefined list order.")}}</small></li>
+                                </ul>
+
+
+
                             </div>
 
                             <hr class="mt-2 mb-0">
 
                             <div class="col-md-12">
-                                <label for="users_auto_assigned_quotes" class="form-label fw-bold">{{__("Users for dropdown in quotes")}} <span class="text-danger">*</span></label>
+                                <label for="users_auto_assigned_quotes" class="form-label fw-bold">{{__("Users to show in quote assign dropdown.")}} <span class="text-danger">*</span></label>
                                 <div class="row">
 
                                     @php
+
+                                        $num = 1;
                                         $users_selected_dropdown_quotes = json_decode($users_selected_dropdown_quotes);
                                         //get column value
                                         $users_selected_dropdown_quotes_value = $users_selected_dropdown_quotes->value;
@@ -93,7 +107,7 @@
                                         <div class="col-md-6">
                                             <div class="form-check form-check-inline">
                                                 <input class="form-check-input" type="checkbox" name="users_selected_dropdown_quotes[]" value="{{$user->id}}" id="users_selected_dropdown_quotes{{$user->id}}" @if(in_array($user->id, $dropdownUserIds)) checked @endif>
-                                                <label class="form-check-label" for="users_selected_dropdown_quotes{{$user->id}}">{{$user->name}} {{ $user->last_name }}</label>
+                                                <label class="form-check-label" for="users_selected_dropdown_quotes{{$user->id}}">({{$num++}}) {{$user->name}} {{ $user->last_name }}</label>
                                             </div>
                                         </div>
                                     @endforeach
