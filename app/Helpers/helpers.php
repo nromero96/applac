103433138dc6b738cbf8c8b190318e3a4392a464
Nilton PC <niltondeveloper96@gmail.com>
2024-09-10 02:48:55 -0500

    Update Logic and rating

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -85,29 +85,35 @@ if (!function_exists('rateQuotation')) {
 
         $fecha_solicitud = Carbon::parse($quotation->created_at)->startOfDay();
 
-        $unasemanadespues = $fecha_solicitud->copy()->addWeeks(1);
-        $dossemanasdespues = $fecha_solicitud->copy()->addWeeks(2);
-        $tressemanasdespues = $fecha_solicitud->copy()->addWeeks(3);
+        $unasemanadespues = $fecha_solicitud->copy()->addDays(7);  // 7 días desde la fecha de solicitud
+        $dossemanasdespues = $fecha_solicitud->copy()->addDays(14); // 14 días desde la fecha de solicitud
+        $tressemanasdespues = $fecha_solicitud->copy()->addDays(21); // 21 días desde la fecha de solicitud
+
 
         //######## Fecha de envío :::::::::::
         if ($quotation->shipping_date) {
             $fecha_envio = Carbon::parse(explode(' to ', $quotation->shipping_date)[0]);
             if (in_array($quotationmodeoftransport, ['Air', 'Ground'])) {
-                if ($fecha_envio->between($fecha_solicitud, $unasemanadespues)) {
-                    $rating += 1;
-                } else if ($fecha_envio->between($unasemanadespues, $dossemanasdespues)) {
-                    $rating += 0.5;
+                if ($fecha_envio->between($fecha_solicitud,  $unasemanadespues)) {
+                    $rating += 2;  //a una semana desde la fecha solicitud
+                } elseif ($fecha_envio->between($unasemanadespues, $dossemanasdespues)){
+                    $rating += 1; //Desde el día 8-14
                 }
-            } else if (in_array($quotationmodeoftransport, ['Container', 'RoRo', 'Breakbulk'])) {
-                if ($fecha_envio->between($fecha_solicitud, $tressemanasdespues)) {
-                    $rating += 1;
+            } elseif (in_array($quotationmodeoftransport, ['Container', 'RoRo', 'Breakbulk'])) {
+                if ($fecha_envio->between($fecha_solicitud, $unasemanadespues)) {
+                    $rating += 2; //a una semana desde la fecha solicitud
+                } elseif($fecha_envio->between($unasemanadespues, $tressemanasdespues)){
+                    $rating += 1; //Hasta 3 semanas desde la fecha solicitud
                 }
             }
         }
 
 
         //######## Correo electrónico :::::::::::
-        $email = $quotation->customer_user_id ? \App\Models\User::find($quotation->customer_user_id)->email : \App\Models\GuestUser::find($quotation->guest_user_id)->email;
+        $email = $quotation->customer_user_id
+        ? \App\Models\User::find($quotation->customer_user_id)->email
+        : \App\Models\GuestUser::find($quotation->guest_user_id)->email;
+
         $domain = substr(strrchr($email, "@"), 1);
 
         $personal_domains = [
@@ -119,14 +125,10 @@ if (!function_exists('rateQuotation')) {
             'icloud.com',
         ];
 
-        // Verificar si el dominio está en la lista de dominios permitidos o si termina en .edu
-        if (!in_array($domain, $personal_domains) && !preg_match('/\.edu(\.[a-z]{2,})?$/', $domain)) {
-            $rating += 1;
-        }
 
-        //######## Origen/Destino y Ubicación :::::::::::
+        //######## Origen/Destino, Ubicación y correo :::::::::::
         $scopeCountries = ['7', '9', '10', '12', '16', '19', '22', '24', '26', '30', '38', '40', '43', '47', '52', '55', '60', '61', '63', '65', '76', '87', '88', '90', '94', '95', '97', '108', '138', '142', '147', '154', '158', '169', '171', '172', '177', '184', '185', '187', '208', '221', '225', '231', '233', '237', '239', '240'];  // Países en el scope
-        $specialCountries = ['38', '231']; // Países especiales (Canadá y EE. UU.)
+        $specialCountries = ['38', '231']; // Países especiales (Canadá y USA)
 
         $location = $quotation->customer_user_id ? \App\Models\User::find($quotation->customer_user_id)->location : \App\Models\GuestUser::find($quotation->guest_user_id)->location;
 
@@ -134,14 +136,27 @@ if (!function_exists('rateQuotation')) {
 
         $isOriginInScope = in_array($quotation->origin_country_id, $scopeCountries);
         $isDestinationInScope = in_array($quotation->destination_country_id, $scopeCountries);
+        $isBusinessEmailAndNotEdu = !in_array($domain, $personal_domains) && !preg_match('/\.edu(\.[a-z]{2,})?$/', $domain);
+
+        //scope exclude $specialCountries
+        $scopeExcludeSpecialCountries = array_diff($scopeCountries, $specialCountries);
+        //Location dentro del Scope $specialCountries
+        $isLocationInScopeExcludeSpecialCountries = in_array($location, $scopeExcludeSpecialCountries);
 
         if ($isLocationInSpecialCountries) {
-            if ($isOriginInScope && $isDestinationInScope) {
-                $rating += 2; // Tanto origen como destino están en el scope
-            } elseif ($isOriginInScope || $isDestinationInScope) {
-                $rating += 1; // Origen o destino están en el scope
-            } else {
-                $rating += 0.5; // Cualquier país fuera del scope
+            if ($isBusinessEmailAndNotEdu) {
+                if ($isOriginInScope && $isDestinationInScope) {
+                    $rating += 2;  // Tanto origen como destino están en el scope y el correo es de una empresa
+                } elseif ($isOriginInScope || $isDestinationInScope) {
+                    $rating += 1; // Origen o destino están en el scope y el correo es de una empresa
+                } else {
+                    $rating += 0.5; // Cualquier país fuera del scope
+                }
+            }
+        // Si la ubicación está en los países del scope excluyendo los especiales
+        } elseif ($isLocationInScopeExcludeSpecialCountries && $isBusinessEmailAndNotEdu) {
+            if (in_array($quotation->origin_country_id, $specialCountries) && in_array($quotation->destination_country_id, $scopeExcludeSpecialCountries)) {
+                $rating += 1; // Origen en specialCountries y destino en scopeExcludeSpecialCountries y el correo es de una empresa
             }
         }
 
@@ -187,49 +202,89 @@ if (!function_exists('rateQuotation')) {
         $quotation->rating = $rating;
         $quotation->save();
 
-        //si asignar a usuario si el rating es menor o igual a 4
-        if($rating <= 4){
+        //si asignar a usuario si el rating es menor o igual a 5
+        if($rating <= 5){
             // Obtener el valor de users_auto_assigned_quotes de la tabla settings
             $users_auto_assigned_quotes = \App\Models\Setting::where('key', 'users_auto_assigned_quotes')->first()->value;
             $userIds = json_decode($users_auto_assigned_quotes);
 
 
+            // Buscar si la cotización es con el mismo email
+            if (in_array($domain, $personal_domains)) {
 
-            // Buscar email en la tabla users el ultimo usuario registrado con el email
-            if (!in_array($domain, $personal_domains)){
+                // Buscar el usuario con el correo electrónico proporcionado
+                $user = \App\Models\User::where('email', $email)->first();
+                $guestUser = \App\Models\GuestUser::where('email', $email)->first();
 
-                // Buscar el antipenúltimo usuario registrado con el mismo dominio en la tabla users
-                $customerwithemail = \App\Models\User::where('email', 'like', "%@$domain")->orderBy('id', 'desc')->skip(1)->first();
-                if ($customerwithemail) {
-                    $quotationwithemail = \App\Models\Quotation::where('customer_user_id', $customerwithemail->id)->orderBy('id', 'desc')->first();
-                    if ($quotationwithemail) {
-                        $quotationwithemailassigned = \App\Models\User::find($quotationwithemail->assigned_user_id);
-                        if ($quotationwithemailassigned) {
-                            $quotation->assigned_user_id = $quotationwithemailassigned->id;
-                            $quotation->save();
-                            return $rating;
-                        }
-                    }
+                // Inicializar variables para almacenar las cotizaciones
+                $quotationWithUser = null;
+                $quotationWithGuest = null;
+
+                if ($user) {
+                    $quotationWithUser = \App\Models\Quotation::where('customer_user_id', $user->id)
+                        ->whereNotNull('assigned_user_id') // Asegúrate de que assigned_user_id no sea nulo
+                        ->orderBy('id', 'desc') // Ordenar por ID descendente
+                        ->first(); // Obtener la última cotización
                 }
 
-                // Buscar el antipenúltimo invitado registrado con el mismo dominio en la tabla guest_users
-                $guestuserwithemail = \App\Models\GuestUser::where('email', 'like', "%@$domain")->orderBy('id', 'desc')->skip(1)->first();
-                if ($guestuserwithemail) {
-                    $quotationwithemailguest = \App\Models\Quotation::where('guest_user_id', $guestuserwithemail->id)->orderBy('id', 'desc')->first();
-                    if ($quotationwithemailguest) {
-                        $quotationwithemailguestassigned = \App\Models\User::find($quotationwithemailguest->assigned_user_id);
-                        if ($quotationwithemailguestassigned) {
-                            $quotation->assigned_user_id = $quotationwithemailguestassigned->id;
-                            $quotation->save();
-                            return $rating;
-                        }
-                    }
+                // Buscar la última cotización basada en el guest_user_id
+                if ($guestUser) {
+                    $quotationWithGuest = \App\Models\Quotation::where('guest_user_id', $guestUser->id)
+                        ->whereNotNull('assigned_user_id') // Asegúrate de que assigned_user_id no sea nulo
+                        ->orderBy('id', 'desc') // Ordenar por ID descendente
+                        ->first(); // Obtener la última cotización
                 }
+
+                // Decidir cuál cotización usar
+                if ($quotationWithUser) {
+                    // Si existe una cotización para el usuario, utilízala
+                    $quotation->assigned_user_id = $quotationWithUser->assigned_user_id;
+                    $quotation->save();
+                    return $rating;
+                } elseif ($quotationWithGuest) {
+                    // Si no hay cotización para el usuario pero sí para el invitado, utilízala
+                    $quotation->assigned_user_id = $quotationWithGuest->assigned_user_id;
+                    $quotation->save();
+                    return $rating;
+                }
+
+            }
+
+            if (!in_array($domain, $personal_domains)) {
+
+                // Buscar la última cotización con el mismo dominio
+                $lastUserQuotationWithDomain = \App\Models\Quotation::join('users as user', 'quotations.customer_user_id', '=', 'user.id')
+                    ->where('user.email', 'like', "%@$domain")
+                    ->whereNotNull('quotations.assigned_user_id') // Asegúrate de que assigned_user_id no sea nulo
+                    ->orderBy('quotations.id', 'desc') // Ordenar por ID descendente
+                    ->select('quotations.assigned_user_id')
+                    ->first();
+
+                // Buscar el último invitado registrado con el mismo dominio
+                $lastGuestQuotationWithDomain = \App\Models\Quotation::join('guest_users as guest', 'quotations.guest_user_id', '=', 'guest.id')
+                    ->where('guest.email', 'like', "%@$domain")
+                    ->whereNotNull('quotations.assigned_user_id') // Asegúrate de que assigned_user_id no sea nulo
+                    ->orderBy('quotations.id', 'desc') // Ordenar por ID descendente
+                    ->select('quotations.assigned_user_id')
+                    ->first();
+
+                if($lastUserQuotationWithDomain){
+                    $quotation->assigned_user_id = $lastUserQuotationWithDomain->assigned_user_id;
+                    $quotation->save();
+                    return $rating;
+                } elseif($lastGuestQuotationWithDomain){
+                    $quotation->assigned_user_id = $lastGuestQuotationWithDomain->assigned_user_id;
+                    $quotation->save();
+                    return $rating;
+                }
+
             }
 
 
-            //ver si la cotización cumple con 4 rating
-            if($rating == 4){
+
+
+            //ver si la cotización cumple con 4 y 5 rating
+            if($rating >= 4){
                 $userQuotationFourRatingCounts = [];
 
                 $now = Carbon::now();
@@ -238,7 +293,7 @@ if (!function_exists('rateQuotation')) {
 
                 foreach ($userIds as $userId) {
                     $userQuotationFourRatingCounts[$userId] = \App\Models\Quotation::where('assigned_user_id', $userId)
-                    ->where('rating', 4)
+                    ->where('rating', '>=', 4)
                     ->whereBetween('created_at', [$yesterday, $now])
                     ->count();
                 }
