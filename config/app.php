e9bc866baf6e7da323b327a54ee75c2ed8858e60
Juan Montoya <jcmv2402@gmail.com>
2025-09-02 23:01:14 -0500

    version updated

--- config/app.php
+++ config/app.php
@@ -14,7 +14,7 @@ return [
     */
 
     'name' => env('APP_NAME', 'Laravel'),
-    'version' => '1.5.0',
+    'version' => '1.5.1',
     /*
     |--------------------------------------------------------------------------
     | Application Environment
