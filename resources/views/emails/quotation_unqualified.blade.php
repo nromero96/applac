f7a2bceb77c53175292007c70aeaa6def6375659
Juan Montoya <jcmv2402@gmail.com>
2025-09-11 09:43:15 -0500

    fix

--- resources/views/emails/quotation_unqualified.blade.php
+++ resources/views/emails/quotation_unqualified.blade.php
@@ -10,7 +10,7 @@
 
     <p style="font-size: 14px; line-height: 1.4;">Thank you for reaching out to LAC. We appreciate the opportunity to be considered for your freight shipping needs.</p>
 
-    <p style="font-size: 14px; line-height: 1.4;">After carefully reviewing your request, we’ve determined that we are not the best fit for your current logistics requirements. As part of our commitment to delivering exceptional service, we occasionally need to make the difficult decision to decline requests that fall outside the focus of our specialized operations.</p>
+    <p style="font-size: 14px; line-height: 1.4;">After carefully reviewing your request, we've determined that we are not the best fit for your current logistics requirements. As part of our commitment to delivering exceptional service, we occasionally need to make the difficult decision to decline requests that fall outside the focus of our specialized operations.</p>
 
     <p style="font-size: 14px; line-height: 1.4;">We truly value your interest, and should your needs evolve in the future, we would be happy to revisit your request.</p>
 
