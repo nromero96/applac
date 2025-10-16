69926cfd2fed332b358f928879dd78911341189d
Juan Montoya <jcmv2402@gmail.com>
2025-10-15 19:03:55 -0500

    fix migration department

--- database/migrations/2025_10_02_130326_create_departments_table.php
+++ database/migrations/2025_10_02_130326_create_departments_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreateDepartmentsTable extends Migration
@@ -18,6 +19,12 @@ class CreateDepartmentsTable extends Migration
             $table->string('name');
             $table->timestamps();
         });
+
+        // Insertar los dos valores fijos
+        DB::table('departments')->insert([
+            ['name' => 'SEO/House'],
+            ['name' => 'Agents'],
+        ]);
     }
 
     /**
