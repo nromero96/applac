a99b7383a5ff20547ebaf2990a0565f24f4d8318
Juan Montoya <jcmv2402@gmail.com>
2025-11-24 17:32:23 -0500

    chunk export excel

--- app/Http/Controllers/QuotationController.php
+++ app/Http/Controllers/QuotationController.php
@@ -387,13 +387,13 @@ class QuotationController extends Controller
             ];
 
             $builder = $quotations; // tu query ya filtrada y ordenada
-            ini_set('max_execution_time', 0);
+            // ini_set('max_execution_time', 0);
 
             $callback = function () use ($builder, $columns) {
                 $file = fopen('php://output', 'w');
                 fputcsv($file, $columns); // Encabezados
 
-                $builder->chunk(1000, function ($rows) use ($file) {
+                $builder->chunk(500, function ($rows) use ($file) {
                     foreach ($rows as $quotation) {
                         fputcsv($file, [
                             $quotation->quotation_id ?? '',
