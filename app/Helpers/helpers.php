760d6b08606f4aa819c43c003a88dac40171df00
Juan Montoya <jcmv2402@gmail.com>
2026-04-21 21:39:47 -0500

    log in rating shipment_ready_date

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1146,7 +1146,7 @@ if (!function_exists('rateQuotationWeb')) {
             }
 
             //ver si la cotización cumple con 4 y 5 rating
-            Log::info('shipment_ready_date: ' . $quotation->shipment_ready_date);
+            Log::info('shipment_ready_date #' . $quotation->id . ': '. $quotation->shipment_ready_date . ', rating: ' . $rating);
             if($rating >= 4 && $quotation->shipment_ready_date == 'Ready to ship now') {
                 // usuarios temporalmente
                 $stephanieId = 2733;
